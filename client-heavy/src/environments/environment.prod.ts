dab0e9ea2474824d38b561df01a1d3439fe296b4
Manuel Ramirez <manuel.ramirez-evrard@polymtl.ca>
2023-02-09 18:13:16 -0500

    AWS server url

--- client-heavy/src/environments/environment.prod.ts
+++ client-heavy/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: "http://10.200.44.52:3000/",
-    //serverUrl: 'http://ec2-15-222-249-186.ca-central-1.compute.amazonaws.com:3000/', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-52-3-241-220.compute-1.amazonaws.com:3000/', // TODO: Add remote server URL
 };
