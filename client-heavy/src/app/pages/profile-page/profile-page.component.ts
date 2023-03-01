56561db1ca10434b64d5ec9b27a7d5ce17ee35c2
Dariadanciu <ioana-daria.danciu@polymtl.ca>
2023-03-01 14:11:55 -0500

    ID: added the profile page

--- client-heavy/src/app/pages/profile-page/profile-page.component.ts
+++ client-heavy/src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+  }
 
   ngOnInit(): void {
   }
 
+  translateLanguageTo(lang: string) {
+    this.translate.use(lang);
+  }
 }
