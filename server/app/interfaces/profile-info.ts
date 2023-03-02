7480550f00abed406027df6ca699310d66fb441f
rak553 <raphael62243@gmail.com>
2023-03-02 16:43:28 -0500

    [Feature] Add the interfaces for the profile info

---
+++ server/app/interfaces/profile-info.ts
@@ -0,0 +1,39 @@
+export enum ConnectionType {
+    CONNECTION,
+    DISCONNECTION,
+}
+
+export interface StatisticInfo {
+    name: string;
+    statAmount: number;
+}
+
+export interface ConnectionInfo {
+    connectionType: ConnectionType;
+    date: string;
+    time: string;
+}
+
+export interface GameInfo {
+    date: string;
+    time: string;
+    length: string;
+    winnerIndex: number;
+    players: PlayerGameInfo[];
+    gameMode: string;
+}
+
+export interface PlayerGameInfo {
+    name: string;
+    score: number;
+}
+
+export interface ProfileInfo {
+    avatar: string;
+    level: number;
+    userCode: string;
+    stats: StatisticInfo[];
+    tournamentWins: number[];
+    connectionHistory: ConnectionInfo[];
+    gameHistory: GameInfo[];
+}
