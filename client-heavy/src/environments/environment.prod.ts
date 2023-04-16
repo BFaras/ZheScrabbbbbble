72a197adb0642dbb80b579c910c3a6b571b71e31
Manuel Ramirez <manuel.ramirez-evrard@polymtl.ca>
2023-04-16 13:07:02 -0400

    Changed server address

--- client-heavy/src/environments/environment.prod.ts
+++ client-heavy/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     //serverUrl: 'http://localhost:3000/',
-    serverUrl: 'http://ec2-54-197-117-26.compute-1.amazonaws.com:3000/',
+    serverUrl: 'http://ec2-18-209-152-53.compute-1.amazonaws.com:3000/',
 };
